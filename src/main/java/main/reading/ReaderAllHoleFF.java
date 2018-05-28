9a135b6fbf73a3e2da8946c2676ca5ab4fbf9419
roro <romandiezpro@gmail.com>
2018-05-28 14:48:35 +0200

    refactoring
    on passe du travail au trou par trou au
    travail de segments par segments

--- src/main/java/main/reading/ReaderAllHoleFF.java
+++ src/main/java/main/reading/ReaderAllHoleFF.java
@@ -1,13 +1,9 @@
 package main.reading;
 
 import java.awt.Color;
-
-import main.Constants;
 import main.controler.ControlerText;
 
-public class ReaderAllHoleFF extends ReaderThread {
-	
-	public Object lock = new Object();
+public class ReaderAllHoleFF extends ReaderThread {// extends ReaderHole{
 
 	public ReaderAllHoleFF(ControlerText controler, int h) {
 		super(controler, h);
@@ -15,119 +11,33 @@ public class ReaderAllHoleFF extends ReaderThread {
 
 	public void run() {
 
-		int firstH = h;
-		
-		controler.removeAllMasks();
-		controler.desactiverFenetreFixe();
-		controler.showPage(controler.getPageOf(h));
+		// on montre uniquement les trous à partir du trou actuel et de cette page
 		controler.showHolesInPage(h);
-		for (int i = 0; i < h; i++) {
-			if(controler.getPageOf(i) == controler.getPageOf(h)) {
-				controler.replaceMaskByWord(i);
-			}
-		}
-		
-		// pour chaque trou
-		while (h < controler.getHolesCount()) {	
-			// si le trou est le premier de son segment
-			if (h != firstH) {
-				// Pour tous les segments après le précédent trou, jusqu'au segment de ce trou compris
-				for (int i = h > 0 ? controler.getPhraseOf(h - 1) + 1 : controler.getPhraseOf(h); i <= controler.getPhraseOf(h); i++) {
-					play(i);
-				}
-			} else {
-				play(controler.getPhraseOf(h));
-			}
-			
-			if(needToDead) {
-				return;
-			}
-			
-			// on montre uniquement les trous à partir du trou actuel et de cette page
-			controler.showHolesInPage(h);
-			// colorier le trou actuel en bleu
-			controler.color(h, controler.getColorBackground() != Color.cyan ? Color.cyan : Color.YELLOW);
-			// active la fenêtre de saisie avec le trou actuel
-			controler.activateInputFenetreFixe(h);
-			
-			if(needToDead) {
-				return;
-			}
-			
-			// tant que la saisie n'est pas juste
-			while (!controler.waitForFillFenetreFixe(h)) {
-				// clignotement et compter une erreur
-				controler.doError();
-			}
-			
-			if(needToDead) {
-				return;
-			}
-			
-			// désactiver la fenêtre de saisie
-			controler.desactiverFenetreFixe();
-			// remplacer le trou par le mot correspondant et replacer tous les masques
-			controler.replaceMaskByWord(h);
-			//on passe au trou suivant
-			h++;
-			/// appel des écouteurs de fin de trou ///
-			for (Runnable r : onHoleEnd) {
-				r.run();
-			}
+		// colorier le trou actuel en bleu ou jaune si le fond est bleu
+		controler.color(h, controler.getColorBackground() != Color.cyan ? Color.cyan : Color.YELLOW);
+		// active la fenêtre de saisie avec le trou actuel
+		controler.activateInputFenetreFixe(h);
+
+		if (needToDead) {return;}
+
+		// tant que la saisie n'est pas juste
+		while (!controler.waitForFillFenetreFixe(h)) {
+			if (needToDead) {return;}
+			controler.doError();
 		}
 
-		// on enlève le dernier trou
-		controler.removeAllMasks();
-		
-		// lire les phrases qui restent : pour tous les segments après le dernier trou
-		for (int i = controler.getPhraseOf(controler.getHolesCount() - 1) + 1; i != controler.getPhrasesCount()- 1; i++) {
-			// on montre la page du segment
-			controler.showPage(controler.getPageOfPhrase(i));
-			
-			if(needToDead) {
-				return;
-			}
-			
-			// lire le fichier audio correspondant à ce segment
-			controler.play(i);
-			
-			if(needToDead) {
-				return;
-			}
-			
-			// attendre le temps de pause nécessaire
-			controler.doWait(controler.getCurrentWaitTime(), Constants.CURSOR_LISTEN);
-			
-			if(needToDead) {
-				return;
-			}
-		}
-		// afficher le compte rendu
-		controler.showReport();
+		if (needToDead) {return;}
 
-	}
-	
-	public void play(int n) {
-		// on montre la page du segment
-		controler.showPage(controler.getPageOfPhrase(n));
+		// désactiver la fenêtre de saisie
+		controler.desactiverFenetreFixe();
+		// remplacer le trou par le mot correspondant et replacer tous les masques
+		controler.replaceMaskByWord(h);
 
-		if (needToDead) {
-			return;
+		/// appel des écouteurs de fin de trou ///
+		for (Runnable r : onHoleEnd) {
+			r.run();
 		}
 
-		// lire le fichier audio correspondant à ce segment
-		controler.play(n);
-
-		if (needToDead) {
-			return;
-		}
-
-		// attendre le temps de pause nécessaire
-		controler.doWait(controler.getCurrentWaitTime(), Constants.CURSOR_LISTEN);
-
-		if (needToDead) {
-			return;
-		}
 	}
 
 }
--- src/main/java/main/reading/ReaderOneHoleUF.java
+++ src/main/java/main/reading/ReaderOneHoleUF.java
@@ -2,60 +2,52 @@ package main.reading;
 
 import main.controler.ControlerText;
 
-public class ReaderOneHoleUF extends ReaderThread {
+public class ReaderOneHoleUF extends ReaderThread { //extends HoleThread {
 	
 	public ReaderOneHoleUF(ControlerText controler, int h) {
 		super(controler, h);
 	}
 	
 	public void run() {
-		while (h < controler.getHolesCount() - 1 && !needToDead) {
-			/// affiche la page correspondante ///
+		
 			int page = controler.getPageOf(h);
-			controler.showPage(page);
-			/// remplace tous les trous de la page///
-			for (int i = 0; i < controler.getPhrasesCount(); i++) {
-				if (controler.getPageOf(i) == page) {
-					controler.fillHole(i);
-				}
-			}
-			/// affiche le trou actuel ///
+			showPage(page);
+			
+			/// affiche uniquement le trou actuel ///
 			controler.showJustHole(h);
-			/// lit tous les segments à lire jusqu'au trou actuel ///
-			if (controler.isFirstInPhrase(h)) {
-				controler.readPhrase(controler.getPhraseOf(h));
-			}
-			if (needToDead) {
-				return;
-			}
+			
+			if (needToDead) {return;}
+			
 			/// attends une saisie de l'utilisateur ///
 			while (!controler.waitForFill(h)) {
-				if (needToDead) {
-					return;
-				}
+				if (needToDead) {return;}
 				controler.doError();
 			}
-			//on efface tous les masques
-			controler.removeAllMasks();
-			h++;
+			
+			if (needToDead) {return;}
+			
+			// remplacer le trou par le mot correspondant et replacer tous les masques
+			controler.replaceMaskByWord(h);
+			
 			/// appel des écouteurs de fin de trou ///
 			for (Runnable r : onHoleEnd) {
 				r.run();
-			}
-			/// si le trou était le dernier du segment lit tous les segments sans trou après ///
-			if (controler.isLastInPhrase(h - 1)) {
-				controler.fillHole(h - 1);
-				for (int i = controler.getPhraseOf(h - 1) + 1; !controler.hasHole(i) && i < controler.getPhrasesCount() - 1 && !needToDead; i++) {
-					controler.readPhrase(i);
-				}
-			}
-		}
-		/// à la fin, affiche le compte rendu ///
-		if (h == controler.getHolesCount() - 1 && !needToDead) {
-			//efface tous les trous
-			controler.removeAllMasks();
+			}	
 			
-			controler.showReport();
+	}
+	
+	/**
+	 * Montre la page du trou h
+	 * Remplace tous les trous de la page par le bon mot
+	 * 
+	 * @param h
+	 */
+	public void showPage(int h) {
+		controler.showPage(controler.getPageOf(h));
+		for (int i = 0; i < controler.getPhrasesCount(); i++) {
+			if (controler.getPageOf(i) == controler.getPageOf(h)) {
+				controler.replaceMaskByWord(i);
+			}
 		}
 	}
 	
